9d9d888c23bb468dc91674509eb16c80b29c1526
Chanuka999 <chanukaranditha99@gmail.com>
2024-09-07 17:13:18 -0700

    new thing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let rate1 = Document.querySelector("rate1");
-let rate2 = Document.querySelector("rate2");
-let resultBtn = document.querySelector("result");
+let rate1 = Document.querySelector(".rate1");
+let rate2 = Document.querySelector(".rate2");
+let resultBtn = document.querySelector(".result");
 let selects = document.querySelectorAll(".options select");
 let sel1 = selects[0];
 let sel2 = selects[1];
